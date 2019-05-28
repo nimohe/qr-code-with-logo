4cdc36704785971972dd6f33392e70a7278d501b
nimohe-work <nimohe@vip.qq.com>
2019-05-28 11:22:46 +0800

    online demo

--- src/toCanvas.js
+++
@@ -1,8 +0,0 @@
-import { renderQrCode } from './draw-canvas'
-import { drawLogo } from './draw-logo'
-
-export const toCanvas = (options) => {
-  return renderQrCode(options)
-    .then(() => options)
-    .then(drawLogo)
-}
--- test/module/entry.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * entry
- * change logs:
- * 2018/2/26 herbluo created
- */
-import './to-canvas'
-import './to-image'
-import './local-image'
